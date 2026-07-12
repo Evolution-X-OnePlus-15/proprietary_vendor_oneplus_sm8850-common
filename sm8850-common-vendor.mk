fa9e037f78e7f356cfc5e24f7963a58e36ca8880
Marco Veldhuizen <marco_veldhuizen@hotmail.com>
2026-07-12 22:32:04 +0200

    sm8850-common: Sync with device tree
    Change-Id: I05c9dbb8c596e235c10596096b9988a228e9a90f

--- sm8850-common-vendor.mk
+++ sm8850-common-vendor.mk
@@ -1054,7 +1054,6 @@ PRODUCT_PACKAGES += \
     init.kernel.post_boot-canoe_5_2 \
     init.kernel.post_boot-canoe_6_1 \
     init.kernel.post_boot-canoe_default_6_2 \
-    init.kernel.post_boot-memory \
     init.kernel.post_boot \
     init.qcom.class_core \
     init.qcom.sensors \
